535f2442b3b80db7795022dcfba9e52bd661d4f2
darmawan <aep.darmawan@gmail.com>
2021-09-20 17:52:01 +0700

    updates done by...

--- apps/modules/kuwu/views/product/css.php
+++
@@ -1,3 +0,0 @@
-<link href="<?= base_url('assets/admin/vendors/bower_components/bootstrap-select/dist/css/bootstrap-select.css'); ?>" rel="stylesheet">
-<link rel="stylesheet" href="<?php echo base_url('assets/admin/vendors/datatables/datatables.min.css'); ?>">
-<link href="<?= base_url('assets/admin/vendors/bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.css'); ?>" rel="stylesheet">
